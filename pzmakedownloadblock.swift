3218281554adafdb4fea6eb621fb081a70fd10d1
Nate Weaver <wevah@derailer.org>
2021-05-10 13:58:37 -0500

    Inital commit

---
+++ pzmakedownloadblock.swift
@@ -0,0 +1,66 @@
+#!/usr/bin/env swift
+
+import Foundation
+import CryptoKit
+
+guard CommandLine.arguments.count > 1 else { exit(1) }
+
+struct Download {
+	
+	var url: URL
+	
+	func htmlString() -> String {
+		let values: URLResourceValues
+
+		do {
+			values = try url.resourceValues(forKeys: [.contentModificationDateKey, .fileSizeKey])
+		} catch {
+			print(error.localizedDescription)
+			exit(1)
+		}
+		
+		let filename = url.lastPathComponent.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+		let downloadName = url.deletingPathExtension().lastPathComponent
+		
+		let isoFormatter = ISO8601DateFormatter()
+		isoFormatter.formatOptions = [.withFullDate, .withDashSeparatorInDate]
+		let isoString = isoFormatter.string(for: values.contentModificationDate!)!
+		
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateStyle = .long
+		dateFormatter.timeStyle = .none
+		let dateString = dateFormatter.string(for: values.contentModificationDate!)!
+		
+		
+		let byteFormatter = ByteCountFormatter()
+		byteFormatter.allowedUnits = [.useBytes]
+		let fileSize = byteFormatter.string(for: values.fileSize!)!
+	
+		return """
+			<p class="download">
+				<span><a href="downloads/\(filename)">\(downloadName)</a></span>
+				<span class="info">(10.9+; Compressed .dmg) [\(fileSize)]</span>
+				<time datetime="\(isoString)">\(dateString)</time>
+				<span class="hash">SHA-256: \(fileHash())</span>
+			</p>
+			"""
+	}
+	
+	func fileHash() -> String {
+		let data = try! Data(contentsOf: url)	
+		let digest = SHA256.hash(data: data)
+				
+		return digest.reduce(into: "") {
+			$0.append(String($1, radix: 16))
+		}
+	}
+	
+}
+
+for file in CommandLine.arguments[1...] {
+	let url = URL(fileURLWithPath: file)
+
+	let download = Download(url: url)
+
+	print(download.htmlString())
+}
